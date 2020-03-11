4d81a4124ee035ee9bc5854d5de83b7f1744df9b
Celeste Thomas <celestesmthomas@gmail.com>
2020-03-11 23:46:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,7 +21,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -38,5 +38,6 @@ shipping_manifest["muskets" => 2]
 shipping_manifest["gun powder" => 4]
 
   # return the shipping_manifest hash below
+  return shipping_manifest
 end
-return shipping_manifest
+
